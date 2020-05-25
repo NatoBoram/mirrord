fd9ec48bd0ab3943f6b314c061b15652cf9e55b9
Nato Boram <nato.boram@gmail.com>
2020-05-25 08:52:17 -0400

    change output to mirrord

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -4,18 +4,18 @@ go clean
 rm -r build
 
 # MacOS
-env GOOS=darwin    GOARCH=386      go build -o build/losgoi-darwin-386
-env GOOS=darwin    GOARCH=amd64    go build -o build/losgoi-darwin-amd64
-env GOOS=darwin    GOARCH=arm      go build -o build/losgoi-darwin-arm
-env GOOS=darwin    GOARCH=arm64    go build -o build/losgoi-darwin-arm64
+env GOOS=darwin    GOARCH=386      go build -o build/mirrord-darwin-386
+env GOOS=darwin    GOARCH=amd64    go build -o build/mirrord-darwin-amd64
+env GOOS=darwin    GOARCH=arm      go build -o build/mirrord-darwin-arm
+env GOOS=darwin    GOARCH=arm64    go build -o build/mirrord-darwin-arm64
 
 # Linux
-env GOOS=linux     GOARCH=386      go build -o build/losgoi-linux-386
-env GOOS=linux     GOARCH=amd64    go build -o build/losgoi-linux-amd64
-env GOOS=linux     GOARCH=arm      go build -o build/losgoi-linux-arm
-env GOOS=linux     GOARCH=arm64    go build -o build/losgoi-linux-arm64
+env GOOS=linux     GOARCH=386      go build -o build/mirrord-linux-386
+env GOOS=linux     GOARCH=amd64    go build -o build/mirrord-linux-amd64
+env GOOS=linux     GOARCH=arm      go build -o build/mirrord-linux-arm
+env GOOS=linux     GOARCH=arm64    go build -o build/mirrord-linux-arm64
 
 # Windows
-env GOOS=windows   GOARCH=386      go build -o build/losgoi-windows-386.exe
-env GOOS=windows   GOARCH=amd64    go build -o build/losgoi-windows-amd64.exe
-env GOOS=windows   GOARCH=arm      go build -o build/losgoi-windows-arm.exe
+env GOOS=windows   GOARCH=386      go build -o build/mirrord-windows-386.exe
+env GOOS=windows   GOARCH=amd64    go build -o build/mirrord-windows-amd64.exe
+env GOOS=windows   GOARCH=arm      go build -o build/mirrord-windows-arm.exe
